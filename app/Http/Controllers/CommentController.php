d5c48bd9e2bf76cca47df5426e8392cdec4e6b6c
Hamid Teimouri <hamidteimouri@outlook.com>
2018-10-02 18:37:37 +0330

    add views and styles

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use App\Post;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function index(Post $post)
+    {
+        return response()->json($post->comments()->with('user')->latest()->get());
+    }
+
+    public function store(Request $request, Post $post)
+    {
+        $comment = $post->comments()->create([
+            'body' => $request->body,
+            'user_id' => auth()->id(),
+        ]);
+        $comment = Comment::where('id', $comment->id)->with('user')->first();
+
+        return $comment->toJson();
+    }
+}
